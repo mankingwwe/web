cf3409e36dcd0b9ba356a5e5bd167241dc7b1969
kevin <kevinhermanns1234@web.de>
2022-01-01 18:54:37 +0100

    added some css (bootstrap) and changed formatting

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,25 @@
+function calender () {
+    $('#datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        autoclose: true,
+        todayHighlight: true,
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        autoclose: true,
+        changeMonth: true,
+        changeYear: true,
+        orientation: "button"
+    });
+
+    $('#datepicker2').datepicker({
+        format: "dd/mm/yyyy",
+        autoclose: true,
+        todayHighlight: true,
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        autoclose: true,
+        changeMonth: true,
+        changeYear: true,
+        orientation: "button"
+    });
+}
